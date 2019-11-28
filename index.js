42801918af7732d96f52980dbd626ec3fa953c24
shadrach <shadrachtemitayo@gmail.com>
2019-11-28 11:33:00 +0100

    feat: add transitions to elements

--- index.js
+++ index.js
@@ -6,39 +6,6 @@ function scrollIt(destination, duration = 200, easing = "linear", callback) {
     },
     easeInQuad(t) {
       return t * t;
-    },
-    easeOutQuad(t) {
-      return t * (2 - t);
-    },
-    easeInOutQuad(t) {
-      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-    },
-    easeInCubic(t) {
-      return t * t * t;
-    },
-    easeOutCubic(t) {
-      return --t * t * t + 1;
-    },
-    easeInOutCubic(t) {
-      return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-    },
-    easeInQuart(t) {
-      return t * t * t * t;
-    },
-    easeOutQuart(t) {
-      return 1 - --t * t * t * t;
-    },
-    easeInOutQuart(t) {
-      return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
-    },
-    easeInQuint(t) {
-      return t * t * t * t * t;
-    },
-    easeOutQuint(t) {
-      return 1 + --t * t * t * t * t;
-    },
-    easeInOutQuint(t) {
-      return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
     }
   };
 
@@ -97,6 +64,31 @@ function scrollIt(destination, duration = 200, easing = "linear", callback) {
   }
 
   scroll();
+}
+
+const transitionElms = [...document.querySelectorAll("[data-transition]")];
+
+if ("IntersectionObserver" in window) {
+  let animationObserver = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let node = entry.target;
+        console.log("entring ", node);
+        node.style.transition = ".5s ease-in";
+        node.classList.remove(node.dataset.transition);
+        animationObserver.unobserve(node);
+      }
+    });
+  });
+
+  transitionElms.forEach(el => {
+    animationObserver.observe(el);
+  });
+} else {
+  transitionElms.forEach(node => {
+    node.style.transition = ".5s ease-in";
+    node.classList.remove(node.dataset.transition);
+  });
 }
 
 let link = document.querySelector("#learn-more");
